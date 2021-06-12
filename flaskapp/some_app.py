0c7b9472c1b13ec40b526fd35321d1490870a1fa
Eugene <59763009+djdiidd@users.noreply.github.com>
2021-06-12 17:22:30 +0700

    Update some_app.py

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -19,3 +19,41 @@ def data_to():
     #передаем данные в шаблон и вызываем его
     return render_template('simple.html',some_str = some_str,
             some_value = some_value,some_pars=some_pars) 
+
+# new
+bootstrap = Bootstrap(app)
+from flask_wtf import FlaskForm,RecaptchaField
+from wtforms import StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
+SECRET_KEY = 'secret'
+app.config['SECRET_KEY'] = SECRET_KEY
+app.config['RECAPTCHA_USE_SSL'] = False
+app.config['RECAPTCHA_PUBLIC_KEY'] = 'сюда поместить ключ из google'
+app.config['RECAPTCHA_PRIVATE_KEY'] = 'сюда поместить секретный ключ из google'
+app.config['RECAPTCHA_OPTIONS'] = {'theme': 'white'}
+from flask_bootstrap import Bootstrap
+bootstrap = Bootstrap(app)
+class NetForm(FlaskForm):
+    openid = StringField('openid', validators = [DataRequired()])
+    upload = FileField('Load image', validators=[
+             FileRequired(),
+             FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+    recaptcha = RecaptchaField()
+    submit = SubmitField('send')
+from werkzeug.utils import secure_filename
+import os
+import net as neuronet
+@app.route("/net",methods=['GET', 'POST'])
+def net():
+    form = NetForm()
+    filename = None
+    neurodic = {}
+    if form.validate_on_submit():
+        filename = os.path.join('./static', secure_filename(form.upload.data.filename))
+        fcount, fimage = neuronet.read_image_files(10,'./static')
+        decode = neuronet.getresult(fimage)
+        for elem in decode:
+            neurodic[elem[0][1]] = elem[0][2]
+        form.upload.data.save(filename)
+    return render_template('net.html',form=form,image_name=filename,neurodic=neurodic)

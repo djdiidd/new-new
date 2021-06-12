2d13c7850d0acf11f628b6658bafd05e129c59e5
Eugene <59763009+djdiidd@users.noreply.github.com>
2021-06-12 21:15:09 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,10 +1,10 @@
 import requests
-# r = requests.get('http://localhost:5000/')
-# print(r.status_code)
-# print(r.text)
-# r = requests.get('http://localhost:5000/data_to') 
-# print(r.status_code)
-# print(r.text) 
+r = requests.get('http://localhost:5000/')
+print(r.status_code)
+print(r.text)
+r = requests.get('http://localhost:5000/data_to') 
+print(r.status_code)
+print(r.text) 
 
 import os
 from io import BytesIO 

7fefe24c1226f89581966b9355a7904ea3415ae1
Eugene <59763009+djdiidd@users.noreply.github.com>
2021-06-13 11:26:30 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -19,12 +19,12 @@ res = requests.post('http://localhost:5000/apinet', json=jsondata)
 if res.ok:
     print(res.json())
 
-# try:
-#     r = requests.get('http://localhost:5000/apixml')
-#     print(r.status_code)
-#     if(r.status_code!=200):
-#         exit(1)
-#     print(r.text)
-# except:
-#     exit(1)
+try:
+    r = requests.get('http://localhost:5000/apixml')
+    print(r.status_code)
+    if(r.status_code!=200):
+        exit(1)
+    print(r.text)
+except:
+    exit(1)
     

5f0b9d2ad861325ac52dabd40acc0b6a957f741a
Eugene <59763009+djdiidd@users.noreply.github.com>
2021-06-13 11:38:14 +0700

    Update client.py

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,9 +1,6 @@
 import requests
 r = requests.get('http://localhost:5000/')
-print(r.status_code)
-print(r.text)
-r = requests.get('http://localhost:5000/data_to') 
-print(r.status_code)
+print(r.status_code) 
 print(r.text) 
 
 import os
